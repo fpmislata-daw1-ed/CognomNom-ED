97429e2fccb0f8e6ab9ad5f66a96c47eccb83624
Joan Puigcerver <j.puigcerveribanez@edu.gva.es>
2025-05-19 17:02:21 +0200

    Math: estructura (#1)

---
+++ src/main/java/ud7/exercises/Math.java
@@ -0,0 +1,12 @@
+package ud7.exercises;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Math {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+        int a = in.nextInt();
+        int b = in.nextInt();
+    }
+}
