07ed6fefadacc09a6fe052e735b30022df10f411
Joan Puigcerver <j.puigcerveribanez@edu.gva.es>
2025-05-19 17:19:58 +0200

    Math: Divisió (#3)

--- src/main/java/ud7/exercises/Math.java
+++ src/main/java/ud7/exercises/Math.java
@@ -7,6 +7,9 @@ public class Math {
     public static int resta(int a, int b){
         return a - b;
     }
+    public static int divisio(int a, int b){
+        return a / b;
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
@@ -14,6 +17,9 @@ public class Math {
         int b = in.nextInt();
 
         int resta = resta(a, b);
+        int divisio = divisio(a, b);
+
+        System.out.println("Divisió:" + divisio);
         System.out.println("Resta: " + resta);
     }
 }
