94423a2c4fb8766a319a80ee44480a21882af74e
Joan Puigcerver <j.puigcerveribanez@edu.gva.es>
2025-05-19 17:08:23 +0200

    suma: main

--- src/main/java/ud7/exercises/Math.java
+++ src/main/java/ud7/exercises/Math.java
@@ -12,5 +12,8 @@ public class Math {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
         int a = in.nextInt();
         int b = in.nextInt();
+
+        int suma = suma(a, b);
+        System.out.println("Suma: " + suma);
     }
 }
