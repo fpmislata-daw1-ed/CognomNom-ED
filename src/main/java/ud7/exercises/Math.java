f8c0649886f9a5b9e9df59b1c79283d367723bdc
Joan Puigcerver <j.puigcerveribanez@edu.gva.es>
2025-05-19 17:16:13 +0200

    Math: resta (#5)

--- src/main/java/ud7/exercises/Math.java
+++ src/main/java/ud7/exercises/Math.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class Math {
+    public static int resta(int a, int b){
+        return a - b;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
         int a = in.nextInt();
         int b = in.nextInt();
+
+        int resta = resta(a, b);
+        System.out.println("Resta: " + resta);
     }
 }
