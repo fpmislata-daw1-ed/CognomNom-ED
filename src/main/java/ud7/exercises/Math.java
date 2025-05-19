63ba7579a8d4e4b3ec30fe2632bb0146618a4125
Joan Puigcerver <j.puigcerveribanez@edu.gva.es>
2025-05-19 17:13:38 +0200

    div: metode + main

--- src/main/java/ud7/exercises/Math.java
+++ src/main/java/ud7/exercises/Math.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class Math {
+    public static int divisio(int a, int b){
+        return a / b;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
         int a = in.nextInt();
         int b = in.nextInt();
+
+        int divisio = divisio(a, b);
+        System.out.println("Divisió:" + divisio);
     }
 }
