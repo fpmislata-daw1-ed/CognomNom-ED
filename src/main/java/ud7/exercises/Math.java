f496fb16643244539b26ee850c52ec31e35efc5a
Joan Puigcerver <j.puigcerveribanez@edu.gva.es>
2025-05-19 17:13:02 +0200

    mult: metode + main

--- src/main/java/ud7/exercises/Math.java
+++ src/main/java/ud7/exercises/Math.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class Math {
+    public static int multiplicacio(int a,  int b){
+        return a * b;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
         int a = in.nextInt();
         int b = in.nextInt();
+
+        int mult = multiplicacio(a, b);
+        System.out.println("Multiplicació: " + mult);
     }
 }
