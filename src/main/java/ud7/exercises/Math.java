9c591f314c03a00cd206299c2ecb59a1143c6e99
Joan Puigcerver <j.puigcerveribanez@edu.gva.es>
2025-05-19 17:23:30 +0200

    Math: multiplicació (#4)

--- src/main/java/ud7/exercises/Math.java
+++ src/main/java/ud7/exercises/Math.java
@@ -10,6 +10,9 @@ public class Math {
     public static int resta(int a, int b){
         return a - b;
     }
+    public static int multiplicacio(int a,  int b){
+        return a * b;
+    }
     public static int divisio(int a, int b){
         return a / b;
     }
@@ -22,9 +25,11 @@ public class Math {
         int suma = suma(a, b);
         int resta = resta(a, b);
         int divisio = divisio(a, b);
+        int mult = multiplicacio(a, b);
 
         System.out.println("Suma: " + suma);
         System.out.println("Resta: " + resta);
+        System.out.println("Multiplicació: " + mult);
         System.out.println("Divisió:" + divisio);
     }
 }
